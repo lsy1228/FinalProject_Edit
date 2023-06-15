1a68d534e8a42d3735e5297c30cd14d5497f9609
Junny990316 <121914842+Junny990316@users.noreply.github.com>
2023-06-15 13:51:11 +0900

    entity 컴럼추가 16일

---
+++ src/main/java/com/kh/iMMUTABLE/entity/Order.java
@@ -0,0 +1,35 @@
+package com.kh.iMMUTABLE.entity;
+
+import com.kh.iMMUTABLE.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "order")
+@Getter @Setter @ToString
+public class Order {
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private int orderId;
+    private int totalPrice;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    private LocalDateTime orderDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItemList = new ArrayList<>();
+}
---
+++ src/main/java/com/kh/iMMUTABLE/entity/OrderItem.java
@@ -0,0 +1,32 @@
+package com.kh.iMMUTABLE.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.aspectj.weaver.ast.Or;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_item")
+@Getter @Setter @ToString
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_item_id")
+    private int orderItemId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
