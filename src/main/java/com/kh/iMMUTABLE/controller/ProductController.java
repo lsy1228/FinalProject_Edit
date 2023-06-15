61b4297cac378fa0c1131f78747bd30360a672db
fearofgod0001 <dlxo8888@gmail.com>
2023-06-15 14:05:26 +0900

    채팅설계

--- src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
@@ -1,4 +1,10 @@
 package com.kh.iMMUTABLE.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // JSON 등 객체로 반환해준다
+@RequestMapping("/chat")
 public class ProductController {
+
 }
