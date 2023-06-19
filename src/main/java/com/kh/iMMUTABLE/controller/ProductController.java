2824d64dfc0da418d75093e3a41259a5926ce79d
d_kyu <dkrara93@naver.com>
2023-06-19 11:17:57 +0900

    product 조회

--- src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
@@ -1,25 +1,33 @@
 package com.kh.iMMUTABLE.controller;
 
-import com.kh.iMMUTABLE.Service.ProductService;
+import com.kh.iMMUTABLE.service.ProductService;
 import com.kh.iMMUTABLE.dto.ProductDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+
 @RestController // JSON 등 객체로 반환해준다
+@Slf4j
+@RequestMapping("/product")
 public class ProductController {
     ProductService productService;
     public ProductController (ProductService itemService){
         this.productService = itemService;
     }
     // 제품 조회
-    @GetMapping("/product")
+    @GetMapping("/item")
     public ResponseEntity<List<ProductDto>> itemList(@RequestParam String name){
         List<ProductDto> list = productService.getProductList();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
+
+
 }
