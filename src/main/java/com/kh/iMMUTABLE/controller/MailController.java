4e07f56697bcbfdf33d98f903fd025b3d800078e
Junny990316 <121914842+Junny990316@users.noreply.github.com>
2023-06-19 17:01:16 +0900

    임시 비밀번호 전송

--- src/main/java/com/kh/iMMUTABLE/controller/MailController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/MailController.java
@@ -28,32 +28,4 @@ public class MailController {
         // ResponseEntity 클라이언트에게 응답을 보낼 때 사용하는 객체
     }
 
-
-//    // 메일인증코드 확인
-//    @PostMapping("/verify") // 주소창에 인증코드가 뜨지 않기 위해 post 방식 사용
-//    public ResponseEntity<Boolean> verifyCode(@RequestBody Map<String, String> mailData) {
-//        String mail = mailData.get("mail"); //키 값이 입력되어 value 값을 출력 할 수 있다.
-//        int code = Integer.parseInt(mailData.get("code"));
-//        System.out.println("메일 : " + mail + ", 코드" + code);
-//        boolean isTrue = mailService.verifyCode(mail, code);
-//        return new ResponseEntity<>(isTrue, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/check/findPw")
-//    public @ResponseBody Map<String, Boolean> pw_find(String userEmail, String userName){
-//        Map<String,Boolean> json = new HashMap<>();
-//        boolean pwFindCheck = UserService.userEmailCheck(userEmail,userName);
-//
-//        System.out.println(pwFindCheck);
-//        json.put("check", pwFindCheck);
-//        return json;
-//    }
-//
-//    //등록된 이메일로 임시비밀번호를 발송하고 발송된 임시비밀번호로 사용자의 pw를 변경하는 컨트롤러
-//    @PostMapping("/check/findPw/sendEmail")
-//    public @ResponseBody void sendEmail(String userEmail, String userName){
-//        MailDto mailDto = mailSendService.createMailAndChangePassword(userEmail, userName);
-//        mailSendService.mailSend(mailDto);
-//
-//    }
 }
--- src/main/java/com/kh/iMMUTABLE/service/MailSendService.java
+++ src/main/java/com/kh/iMMUTABLE/service/MailSendService.java
@@ -3,12 +3,18 @@ package com.kh.iMMUTABLE.service;
 import com.kh.iMMUTABLE.dto.MailDto;
 import com.kh.iMMUTABLE.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
+
+@Slf4j
 @Service // 스프링에서 비즈니스 로직을 처리하는 클래스임을 표시
 @RequiredArgsConstructor // 생성자를 자동으로 생성
 public class MailSendService {
@@ -57,36 +63,49 @@ public class MailSendService {
         return number;
     }
 
-//    // 임시 메일 발송
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    public MailDto createMailAndChangePassword(String userEmail, String userName) {
-//        String str = getTempPassword();
-//        MailDto mailDto = new MailDto();
-//        mailDto.setReceiver(userEmail);
-//        mailDto.setTitle(userName + "님의 iMMUTABLE 임시비밀번호 안내 이메일 입니다.");
-//        mailDto.setContent("\"안녕하세요. iMMUTABLE 임시비밀번호 안내 관련 이메일 입니다.\" + \"[\" + userName + \"]\" +\"님의 임시 비밀번호는 \"\n" +
-//                "        + str + \" 입니다.");
-//        updatePassword(str, userEmail);
-//        return mailDto;
-//    }
-//    public void updatePassword(String str, String userEmail) {
-//        int userId = userRepository.findByUserEmailAndUserPwd(userEmail).getId();
-//        userRepository.updateUserPassword(userId, userPassword);
-//    }
-//
-//    public String getTempPassword(){
-//        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-//                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-//
-//        String str = "";
-//
-//        int idx = 0;
-//        for (int i = 0; i < 10; i++) {
-//            idx = (int) (charSet.length * Math.random());
-//            str += charSet[idx];
-//        }
-//        return str;
-//    }
+    // 임시 비밀번호
+    public MimeMessage resetPwdMessage(String to, String pwd) throws MessagingException, UnsupportedEncodingException {
+        createKey();
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.addRecipients(Message.RecipientType.TO, to); // 보내는 대상
+        message.setSubject("이메일 인증");
+        String msgs = "";
+        msgs += "<div style='margin:100px;'>";
+        msgs += "<h1> 안녕하세요?</h1>";
+        msgs += "<h1> 평범한 식사도 허투루 할 수 없는 당신을 위해, 허당입니다</h1>";
+        msgs += "<br>";
+        msgs += "<p>임시 비밀번호를 발급해드렸습니다. 마이페이지에서 비밀번호를 새롭게 변경해주세요.<p>";
+        msgs += "<br>";
+        msgs += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgs += "<h3 style='color:blue;'>임시 비밀번호 발급 </h3>";
+        msgs += "<div style='font-size:130%'>";
+        msgs += "임시 비밀번호 : <strong>";
+        msgs += pwd + "</strong><div><br/> "; // 메일에 인증번호 넣기
+        msgs += "</div>";
+        message.setText(msgs, "utf-8", "html");// 내용, charset 타입, subtype
+        // 보내는 사람의 이메일 주소, 보내는 사람 이름
+        message.setFrom(new InternetAddress("aelmusic1234@naver.com", "iMMUTABLE"));// 보내는 사람
+        return message;
+    }
+
+    // 인증코드를 만드는 메소드
+    public String createKey() {
+        char[] charSet = new char[]{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+        String pwd = "";
+
+        /* 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 조합 */
+        int idx = 0;
+        for(int i = 0; i < 10; i++){
+            idx = (int) (charSet.length * Math.random());
+            pwd += charSet[idx];
+        }
+
+        log.info("임시 비밀번호 생성");
+
+        return pwd;
+    }
+
 }
