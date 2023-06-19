28e4003e41db45ddd0276cc3b08f1981d5dbbeeb
fearofgod0001 <dlxo8888@gmail.com>
2023-06-19 13:25:47 +0900

    0619_아이템업로드

--- src/main/java/com/kh/iMMUTABLE/service/ProductService.java
+++ src/main/java/com/kh/iMMUTABLE/service/ProductService.java
@@ -1,33 +1,27 @@
 package com.kh.iMMUTABLE.service;
 
-<<<<<<< HEAD
 
 import com.kh.iMMUTABLE.dto.ProductDto;
 import com.kh.iMMUTABLE.entity.Product;
-=======
-import com.kh.iMMUTABLE.constant.Authority;
-import com.kh.iMMUTABLE.entity.Product;
-import com.kh.iMMUTABLE.entity.User;
->>>>>>> 98cba00 (0619_아이템업로드)
+
+
 import com.kh.iMMUTABLE.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 import java.util.List;
-=======
-import java.time.LocalDateTime;
->>>>>>> 98cba00 (0619_아이템업로드)
+
 
 @Service
 @Transactional
 @Slf4j
 @RequiredArgsConstructor
 public class ProductService {
-<<<<<<< HEAD
+
     // 의존성을 통해 빈에 등록된 필드는 불변성이 있어야 하므로 final 선언을 해야 함
     private final ProductRepository productRepository;  // 상품 정보를 조회하는 데이터 액세스 객체
     // 상품정보조회
@@ -40,15 +34,13 @@ public class ProductService {
             productDTO.setProductPrice(product.getProductPrice());
             productDTO.setProductDesc(product.getProductDesc());
             productDTO.setProductMainImg(product.getProductMainImg());
-            productDTO.setProductDetailImg(product.getProductDetailImg());
+            productDTO.setProductDetailImg(product.getProductDetail());
             productDTOS.add(productDTO);
         }
         return  productDTOS;
 
     }
 
-=======
-    private final ProductRepository productRepository;
 
     public boolean itemUpLoad(String productName, String productPrice, String productColor, String productSize,String productCategory,String productMainImg,String productDetail) {
         Product product = new Product();
@@ -62,5 +54,5 @@ public class ProductService {
         Product upLoadItem = productRepository.save(product);
         return true;
     }
->>>>>>> 98cba00 (0619_아이템업로드)
+
 }
