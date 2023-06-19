639e4d79dfa837a474e3ad8bbf7e55f17948af1b
d_kyu <dkrara93@naver.com>
2023-06-19 13:43:00 +0900

    product 수정

--- src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/ProductController.java
@@ -26,7 +26,19 @@ public class ProductController {
         return new ResponseEntity<>(list, HttpStatus.OK);
 
     }
-
+    @PostMapping("/upload")
+    public ResponseEntity<Boolean> uploadItem (@RequestBody Map<String, String> loginData){
+        String productName = loginData.get("productName");
+        String productPrice = loginData.get("productPrice");
+        String productColor = loginData.get("productColor");
+        String productSize = loginData.get("productSize");
+        String productCategory = loginData.get("productCategory");
+        String productMainImg = loginData.get("productMainImg");
+        String productDetail = loginData.get("productDetail");
+        System.out.println(productDetail);
+        boolean result = productService.itemUpLoad(productName,productPrice,productColor,productSize,productCategory,productMainImg,productDetail);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 
 
 }
