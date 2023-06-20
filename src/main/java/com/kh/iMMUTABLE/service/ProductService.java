77b2ad22fdaaa990b182b0523f8480c413651506
LeeSoYoung <258735@naver.com>
2023-06-20 14:57:30 +0900

    0620

--- src/main/java/com/kh/iMMUTABLE/service/ProductService.java
+++ src/main/java/com/kh/iMMUTABLE/service/ProductService.java
@@ -24,20 +24,7 @@ public class ProductService {
 
     // 의존성을 통해 빈에 등록된 필드는 불변성이 있어야 하므로 final 선언을 해야 함
     private final ProductRepository productRepository;  // 상품 정보를 조회하는 데이터 액세스 객체
-    // 상품정보조회
-    public List<ProductDto> getProductList(){
-        List<Product> products = productRepository.findAll();   // 모든 상품 정보 조회
-        List<ProductDto> productDTOS = new ArrayList<>();
-        for(Product product : products){
-            ProductDto productDTO = new ProductDto();
-            productDTO.setProductName(product.getProductName());
-            productDTO.setProductPrice(product.getProductPrice());
-            productDTO.setProductMainImg(product.getProductMainImg());
-            productDTO.setProductDetail(product.getProductDetail());
-            productDTOS.add(productDTO);
-        }
-        return  productDTOS;
-    }
+
     // 전체 상품
     public List<ProductDto> getProduct() {
         List<Product> products = productRepository.findAll();
@@ -46,6 +33,8 @@ public class ProductService {
             ProductDto productDto = new ProductDto();
             productDto.setProductId(product.getProductId());
             productDto.setProductName(product.getProductName());
+            productDto.setProductMainImg(product.getProductMainImg());
+            productDto.setProductDetail(product.getProductDetail());
             productDto.setProductPrice(product.getProductPrice());
             productDtos.add(productDto);
         }
