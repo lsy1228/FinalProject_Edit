6c2843dc4f39519b78f6b57e8d47ff9bc691c3af
fearofgod0001 <dlxo8888@gmail.com>
2023-06-20 12:42:24 +0900

    0620_QNA

---
+++ src/main/java/com/kh/iMMUTABLE/repository/QnaRepository.java
@@ -0,0 +1,7 @@
+package com.kh.iMMUTABLE.repository;
+
+import com.kh.iMMUTABLE.entity.Qna;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaRepository extends JpaRepository<Qna, Long> {
+}
