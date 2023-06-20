4c410501d385f759525334e283e715b0d29eda1b
fearofgod0001 <dlxo8888@gmail.com>
2023-06-20 14:52:15 +0900

    0620_QNA수정

--- src/main/java/com/kh/iMMUTABLE/repository/QnaRepository.java
+++ src/main/java/com/kh/iMMUTABLE/repository/QnaRepository.java
@@ -2,6 +2,13 @@ package com.kh.iMMUTABLE.repository;
 
 import com.kh.iMMUTABLE.entity.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QnaRepository extends JpaRepository<Qna, Long> {
+
+    @Modifying
+    @Query("UPDATE Question q set q.showCount = q.showCount + 1 where q.questionId = :questionId")
+    void updateQnaReply(@Param("questionId") Long questionId);
 }
