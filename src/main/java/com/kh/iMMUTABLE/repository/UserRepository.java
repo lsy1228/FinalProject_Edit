4235b16ec7a001e5b9dc79c1ce5038e831f1ffb4
Junny990316 <heojueun0316@gmail.com>
2023-06-20 15:04:11 +0900

    jueun

--- src/main/java/com/kh/iMMUTABLE/repository/UserRepository.java
+++ src/main/java/com/kh/iMMUTABLE/repository/UserRepository.java
@@ -2,13 +2,15 @@ package com.kh.iMMUTABLE.repository;
 
 import com.kh.iMMUTABLE.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUserEmailAndUserPwd(String userEmail,String userPassword);
 
-
-
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
