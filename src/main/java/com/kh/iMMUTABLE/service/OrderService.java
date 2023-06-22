5ce557111b606aedd72a7becb810974084c97a71
fearofgod0001 <dlxo8888@gmail.com>
2023-06-22 12:42:32 +0900

    0622_order수정

--- src/main/java/com/kh/iMMUTABLE/service/OrderService.java
+++ src/main/java/com/kh/iMMUTABLE/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.kh.iMMUTABLE.service;
 
 import com.kh.iMMUTABLE.constant.OrderStatus;
+import com.kh.iMMUTABLE.dto.OrderDto;
 import com.kh.iMMUTABLE.entity.Order;
 import com.kh.iMMUTABLE.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,9 +19,22 @@ import java.util.List;
 public class OrderService {
     private final OrderRepository orderRepository;
 
-    public List<Order> getOrderListAll() {
+    public List<OrderDto> getOrderListAll() {
         List<Order> orderList = orderRepository.findAll();
-        return orderList;
+        List<OrderDto> orderDtos = new ArrayList<>();
+        for(Order order : orderList){
+            OrderDto orderDto = new OrderDto();
+            orderDto.setOrderId(order.getOrderId());
+            orderDto.setUserId((int) order.getUser().getUserId());
+            orderDto.setOrderAddress(order.getOrderAddress());
+            orderDto.setOrderDate(order.getOrderDate());
+            orderDto.setTotalPrice(order.getTotalPrice());
+            orderDto.setOrderStatus(order.getOrderStatus());
+            orderDto.setShipCompany(order.getShipCompany());
+            orderDto.setShipCode(order.getShipCode());
+            orderDtos.add(orderDto);
+        }
+        return orderDtos;
     }
 
     public boolean upLoadData(int orderId,String orderStatue,int shipCode, String orderShipCompany) {
