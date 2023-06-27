6bb2f1d98053ca5d215a8ff04f83e9563af020f4
heojueun <heojueun0316@gmail.com>
2023-06-27 15:21:17 +0900

    jwt token / faq 조회

--- src/main/java/com/kh/iMMUTABLE/controller/FaqController.java
+++ src/main/java/com/kh/iMMUTABLE/controller/FaqController.java
@@ -1,5 +1,6 @@
 package com.kh.iMMUTABLE.controller;
 
+import com.kh.iMMUTABLE.entity.Faq;
 import com.kh.iMMUTABLE.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,13 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @RestController // JSON 등 객체로 반환해준다
 @Slf4j
 @RequestMapping("/faq")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
 public class FaqController {
     private final FaqService faqService;
 
@@ -22,8 +24,16 @@ public class FaqController {
     public ResponseEntity<Boolean> uploadFaq (@RequestBody Map<String, String> faqData) {
         String faqTitle = faqData.get("faqTitle");
         String faqContent = faqData.get("faqContent");
+        LocalDateTime faqDate = LocalDateTime.now();
         System.out.println(faqTitle);
-        boolean result = faqService.faqUpload(faqTitle, faqContent);
+        boolean result = faqService.faqUpload(faqTitle, faqContent, faqDate);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // faq 가져오기
+    @GetMapping("/faqList")
+    public ResponseEntity<List<Faq>> listFaq () {
+        List<Faq> result = faqService.faqList();
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- src/main/java/com/kh/iMMUTABLE/service/FaqService.java
+++ src/main/java/com/kh/iMMUTABLE/service/FaqService.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,11 +18,24 @@ import java.time.LocalDateTime;
 public class FaqService {
     private final FaqRepository faqRepository;
 
-    public boolean faqUpload(String faqTitle, String faqContent){
+    // faq 업로드
+    public boolean faqUpload(String faqTitle, String faqContent, LocalDateTime faqDate){
         Faq faq = new Faq();
         faq.setFaqTitle(faqTitle);
         faq.setFaqContent(faqContent);
+        faq.setFaqDate(faqDate);
         Faq uploadFaq = faqRepository.save(faq);
         return true;
+    }
+
+//     faq 목록 가져오기
+    public List<Faq> faqList() {
+        List<Faq> faqList = faqRepository.findAll();
+        for(Faq faq : faqList) {
+            faq.getFaqTitle();
+            faq.getFaqContent();
+
+        }
+        return faqList;
     }
 }
