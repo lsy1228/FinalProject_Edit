06bdc145f11edf54ed12d047a9e9edc67be7279e
d_kyu <dkrara93@naver.com>
2023-06-28 15:16:08 +0900

    230628

--- src/main/java/com/kh/iMMUTABLE/repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kh.iMMUTABLE.repository;
-
-import com.kh.iMMUTABLE.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
-    CartItem findByCartIdAndProductId(int cartId, long productId);
-}
