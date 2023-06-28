d916a856904b38c4b8b75e9153f90b6dbf703805
fearofgod0001 <dlxo8888@gmail.com>
2023-06-28 09:06:26 +0900

    0628_upload수정

--- src/main/java/com/kh/iMMUTABLE/entity/Product.java
+++ src/main/java/com/kh/iMMUTABLE/entity/Product.java
@@ -5,12 +5,10 @@ import com.kh.iMMUTABLE.constant.SizeStatus;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,38 +21,28 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private int productId;
-
     @Column(nullable = false)
     private String productName;     // 상품명
-
     @Column(nullable = false)
     private int productPrice;       // 상품 가격
-
     @Column(nullable = false)
     private String productColor;      // 상품 컬러
-
     @Column(nullable = false)
     private String productCategory;      // 상품 카테고리
-
     @Lob
-    private String productMainImg;      // 상품 이미지
-
-
+    private String productImgFst;      // 상품 이미지
+    @Lob
+    private String productImgSnd;      // 상품 이미지
+    @Lob
+    private String productDetail;    // 상품 상세설명
     @Lob
     @Column(nullable = false)
-    private String productDetail;    // 상품 상세
-
+    private String productImgDetail;    // 상품 상세설명
     private int productStock;       // 상품 재고
-
     @Enumerated(EnumType.STRING)
     private ProductSellStatus productSellStatus;    // 상품 판매 상태
-    private LocalDateTime regTime;          // 등록시간
-    private LocalDateTime updateTime;       // 수정시간
-
     @Enumerated(EnumType.STRING)
     private SizeStatus sizeStatus;
-
-
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Qna> qnaList = new ArrayList<>();
