c535100ebac74ce77879b2eca32bc2886df56304
hyeon51 <84119059+hyeon51@users.noreply.github.com>
2023-07-03 09:59:11 +0900

    230703 cart

--- src/main/java/com/kh/iMMUTABLE/entity/Cart.java
+++ src/main/java/com/kh/iMMUTABLE/entity/Cart.java
@@ -25,6 +25,7 @@ public class Cart {
     @JoinColumn(name = "user_id")
     private User user;
 
+
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CartItem> cartItemList = new ArrayList<>();
 
--- src/main/java/com/kh/iMMUTABLE/repository/CartRepository.java
+++ src/main/java/com/kh/iMMUTABLE/repository/CartRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, String> {
     Cart findByUserUserEmail(String userEmail);
+
 }
