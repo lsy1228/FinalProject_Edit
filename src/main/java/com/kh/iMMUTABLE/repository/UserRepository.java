12141c9ce68f94de1b1a8799e564664b3f44d944
fearofgod0001 <dlxo8888@gmail.com>
2023-07-05 12:32:43 +0900

    Users변경

--- src/main/java/com/kh/iMMUTABLE/repository/UserRepository.java
+++ src/main/java/com/kh/iMMUTABLE/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.kh.iMMUTABLE.repository;
 
-import com.kh.iMMUTABLE.dto.UserDto;
 import com.kh.iMMUTABLE.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
