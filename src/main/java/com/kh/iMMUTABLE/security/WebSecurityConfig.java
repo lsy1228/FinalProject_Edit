ab4444cfbcac56abf8117fbdef77cc786debd0d8
fearofgod0001 <dlxo8888@gmail.com>
2023-07-07 12:16:44 +0900

    0707 권한 설정 변경 및 엑시오스 수정

--- src/main/java/com/kh/iMMUTABLE/security/WebSecurityConfig.java
+++ src/main/java/com/kh/iMMUTABLE/security/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/static/**","/auth/**", "/faq/**","/product/**", "/email/**", "/verify/**", "/like/**", "/qna/**","/cart/**", "/order/**","/AdminSignUp/**").permitAll()
+                .antMatchers("/", "/static/**","/auth/**", "/faq/**","/product/**", "/email/**", "/verify/**", "/like/**", "/qna/**","/cart/**", "/order/**","/AdminSignUp/**","/AdminPage/**").permitAll()
                 .antMatchers("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**", "/swagger/**", "/sign-api/exception").permitAll()
                 .anyRequest().authenticated()
 
