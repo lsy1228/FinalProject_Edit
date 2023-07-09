0796cf246313feec4612401495add8d580ae1f68
LEETAESUK <dlxo8888@gmail.com>
2023-07-09 17:04:15 +0900

    0709_웹소켓 핸들러

---
+++ src/main/java/com/kh/iMMUTABLE/handler/WebSocketHandler.java
@@ -0,0 +1,11 @@
+package com.kh.iMMUTABLE.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class WebSocketHandler {
+}
