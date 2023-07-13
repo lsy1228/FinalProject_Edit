4b8a8a1efaf576faaf1797ec4910ab2e7ba3e083
LEETAESUK <dlxo8888@gmail.com>
2023-07-13 19:13:23 +0900

    0713_채팅창수정

--- src/main/frontend/src/chatPage/ChatSocket.js
+++ src/main/frontend/src/chatPage/ChatSocket.js
@@ -149,6 +149,7 @@ const ChatSocket = () => {
             "message":"종료 합니다."}));
         ws.current.close();
         window.localStorage.removeItem("chatRoomId");
+        alert("채팅을 종료합니다.")
     }
 
     useEffect(() => {
